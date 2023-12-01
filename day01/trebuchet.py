8880afd00e664413c63b3b90dcadbe818d76c1f3
Walt Mankowski <waltman@pobox.com>
2023-12-01 06:18:05 -0500

    updated code to solve part 2

--- day01/trebuchet.py
+++ day01/trebuchet.py
@@ -1,12 +1,42 @@
 from sys import argv
+import re
 
-calibration_total = 0
+def calibrate(line):
+    digits = [c for c in line if c.isdigit()]
+    return int(digits[0]) * 10 + int(digits[-1]) if digits else 0
+
+def transform(line):
+    vals = {'one':   '1',
+            'two':   '2',
+            'three': '3',
+            'four':  '4',
+            'five':  '5',
+            'six':   '6',
+            'seven': '7',
+            'eight': '8',
+            'nine':  '9',
+            }
+
+    ss = line + '    '
+    digits = []
+    for i in range(len(ss)):
+        if ss[i].isdigit():
+            digits.append(ss[i])
+        else:
+            for k, v in vals.items():
+                if ss[i:i+len(k)] == k:
+                    digits.append(v)
+    return digits
+
+calibration_total1 = 0
+calibration_total2 = 0
 
 with open(argv[1]) as f:
     for line in f:
-        digits = [c for c in line if c.isdigit()]
-        calibration = int(digits[0]) * 10 + int(digits[-1])
-        calibration_total += calibration
+        line = line.rstrip()
+        calibration_total1 += calibrate(line)
+        calibration_total2 += calibrate(transform(line))
 
-print('Part 1:', calibration_total)
+print('Part 1:', calibration_total1)
+print('Part 2:', calibration_total2)
 
