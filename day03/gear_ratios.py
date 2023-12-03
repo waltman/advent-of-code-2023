f1a29be1b03dbcb55508bd5cd5089963b3bd9c6c
Walt Mankowski <waltman@pobox.com>
2023-12-03 07:32:46 -0500

    added code to solve part 2

--- day03/gear_ratios.py
+++ day03/gear_ratios.py
@@ -23,6 +23,8 @@ cols += 2
 
 # now look for numbers in the grid
 part1 = 0
+part2 = 0
+part_range = []
 for row in range(1, rows-1):
     col = 1
     start_col = -1
@@ -37,11 +39,22 @@ for row in range(1, rows-1):
             if start_col != -1:
                 if is_part_num(grid, row, start_col, col-1):
                     part1 += val
+                    part_range.append((val, row-1, row+1, start_col-1, col))
                 start_col = -1
                 val = -1
         col += 1
         
 print('Part 1:', part1)
 
+# now look for gears
+for row in range(1, rows-1):
+    for col in range(1, cols-1):
+        if grid[row,col] == '*':
+            vals = []
+            for val, start_row, end_row, start_col, end_col in part_range:
+                if start_row <= row <= end_row and start_col <= col <= end_col:
+                    vals.append(val)
+            if len(vals) == 2:
+                part2 += vals[0] * vals[1]
 
-
+print('Part 2:', part2)
