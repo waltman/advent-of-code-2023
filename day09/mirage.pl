361703750ca8cf5d98792c19a8d7ec975b715fac
Walt Mankowski (macbook pro) <waltman@pobox.com>
2023-12-09 07:55:09 -0500

    perl code for day 9
    Nice chance to play with map(), any() and postfix dereferencing today.

---
+++ day09/mirage.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+use v5.38;
+use List::Util qw(any sum);
+
+my @reports;
+while (<>) {
+    chomp;
+    push @reports, [split];
+
+}
+printf "Part 1: %d\n", sum(map {next_val($_)} @reports);
+printf "Part 2: %d\n", sum(map {prev_val($_)} @reports);
+
+sub next_val($report) {
+    my @seqs = ($report);
+    push @seqs, [map { $seqs[-1][$_+1] - $seqs[-1][$_] } 0..$seqs[-1]->$#* - 1] while any {$_} $seqs[-1]->@*;
+
+    my $delta = 0;
+    for (my $i = $#seqs - 1; $i >= 0; $i--) {
+	$delta = $seqs[$i][-1] + $delta;
+    }
+
+    return $delta;
+}
+
+sub prev_val($report) {
+    my @seqs = ($report);
+    push @seqs, [map { $seqs[-1][$_+1] - $seqs[-1][$_] } 0..$seqs[-1]->$#* - 1] while any {$_} $seqs[-1]->@*;
+
+    my $delta = 0;
+    for (my $i = $#seqs - 1; $i >= 0; $i--) {
+	$delta = $seqs[$i][0] - $delta;
+    }
+
+    return $delta;
+}
+
