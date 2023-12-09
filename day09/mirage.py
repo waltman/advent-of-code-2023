0745e2e8539dbb55ce2fc1f9e4de9521f88ca8ab
Walt Mankowski (macbook pro) <waltman@pobox.com>
2023-12-09 06:51:27 -0500

    updated to solve part 2

--- day09/mirage.py
+++ day09/mirage.py
@@ -11,6 +11,17 @@ def next_val(report):
 
     return delta
 
+def prev_val(report):
+    seqs = [report]
+    while any(seqs[-1]):
+        seqs.append([seqs[-1][i+1] - seqs[-1][i] for i in range(len(seqs[-1])-1)])
+
+    delta = 0
+    for i in range(len(seqs)-2, -1, -1):
+        delta = seqs[i][0] - delta
+
+    return delta
+
 def main():
     with open(sys.argv[1]) as f:
         reports = [[int(d) for d in line.split()] for line in f]
@@ -19,5 +30,6 @@ def main():
         next_val(report)
 
     print('Part 1:', sum([next_val(report) for report in reports]))
+    print('Part 2:', sum([prev_val(report) for report in reports]))
 
 main()
