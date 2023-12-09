351fd6e42e5130a1f697906eaa4923a96fff7c42
Walt Mankowski (macbook pro) <waltman@pobox.com>
2023-12-09 06:43:52 -0500

    python code for day 9 part 1

---
+++ day09/mirage.py
@@ -0,0 +1,23 @@
+import sys
+
+def next_val(report):
+    seqs = [report]
+    while any(seqs[-1]):
+        seqs.append([seqs[-1][i+1] - seqs[-1][i] for i in range(len(seqs[-1])-1)])
+
+    delta = 0
+    for i in range(len(seqs)-2, -1, -1):
+        delta = seqs[i][-1] + delta
+
+    return delta
+
+def main():
+    with open(sys.argv[1]) as f:
+        reports = [[int(d) for d in line.split()] for line in f]
+
+    for report in reports:
+        next_val(report)
+
+    print('Part 1:', sum([next_val(report) for report in reports]))
+
+main()
