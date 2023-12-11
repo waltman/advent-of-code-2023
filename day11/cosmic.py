9496e807ddd26025ea0bc41de92a0973cd20447e
Walt Mankowski <mankowsw@pennmedicine.upenn.edu>
2023-12-11 08:50:39 -0500

    added code to solve day 11 part 2

--- day11/cosmic.py
+++ day11/cosmic.py
@@ -20,6 +20,22 @@ def expand_grid(grid):
 
     return new_grid
 
+def expand_galaxies(grid, val):
+    galaxies = [[row,col] for (row,col) in product(range(grid.shape[0]), range(grid.shape[1])) if grid[row][col] == '#']
+    for row in range(grid.shape[0]-1, -1, -1):
+        if np.all(grid[row,:] == '.'):
+            for galaxy in galaxies:
+                if galaxy[0] > row:
+                    galaxy[0] += val
+
+    for col in range(grid.shape[1]-1, -1, -1):
+        if np.all(grid[:,col] == '.'):
+            for galaxy in galaxies:
+                if galaxy[1] > col:
+                    galaxy[1] += val
+
+    return galaxies
+
 def dist(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
@@ -27,9 +43,9 @@ def main():
     with open(sys.argv[1]) as f:
         grid = np.array([[c for c in line.rstrip()] for line in f])
 
-    grid = expand_grid(grid)
-
-    galaxies = [(row,col) for (row,col) in product(range(grid.shape[0]), range(grid.shape[1])) if grid[row][col] == '#']
+    galaxies = expand_galaxies(grid, 1)
     print('Part 1:', sum([dist(p1, p2) for (p1, p2) in combinations(galaxies, 2)]))
+    galaxies = expand_galaxies(grid, 999_999)
+    print('Part 2:', sum([dist(p1, p2) for (p1, p2) in combinations(galaxies, 2)]))
 
 main()
