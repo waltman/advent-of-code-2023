aa3dd5630886935beddaa0daf8ca5cd681083c0a
Walt Mankowski <waltman@pobox.com>
2023-12-12 11:00:46 -0500

    part 2 [WIP]
    I can't solve it this way, but at least I've got the logic for
    expanding the conditions and regex.

--- day12/hot_springs.py
+++ day12/hot_springs.py
@@ -11,25 +11,32 @@ def qm_pos(s):
 
 def matches(condition, size_re):
     idxs = qm_pos(condition)
+    print('in matches, len =', len(idxs))
     arrange = [c for c in condition]
     cnt = 0
     for prod in product('.#', repeat=len(idxs)):
         for i in range(len(idxs)):
             arrange[idxs[i]] = prod[i]
         if re.match(size_re, ''.join(arrange)):
-#            print('match!', size_re, ''.join(arrange))
             cnt += 1
 
     return cnt
 
 def main():
     part1 = 0
+    part2 = 0
     with open(sys.argv[1]) as f:
         for line in f:
             condition, sizes = line.rstrip().split(' ')
             size_re = size_regexp(sizes)
             part1 += matches(condition, size_re)
+            sizes2 = ','.join([str(d) for d in [int(d) for d in sizes.split(',')] * 5])
+            sizes2_re = size_regexp(sizes2)
+            condition2 = '?'.join([condition]*5)
+            print(condition2, sizes, sizes2, sizes2_re)
+            part2 += matches(condition2, sizes2_re)
 
     print('Part 1:', part1)
+    print('Part 2:', part2)
 
 main()
