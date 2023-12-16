915b91f46e98022618c4608d7f93111ae06bec84
Walt Mankowski (macbook pro) <waltman@pobox.com>
2023-12-16 07:02:55 -0500

    code for day 13 part 1 (WIP)
    This works for the test input but is too low for my input.
    
    It says 29321 is too low.

---
+++ day13/incidence.py
@@ -0,0 +1,58 @@
+import sys
+import numpy as np
+
+def score(pattern):
+    # test columns
+    for c in range(pattern.shape[1] - 1):
+        print(c)
+        if np.all(pattern[:,c] == pattern[:,c+1]):
+            ok = True
+            print(f'checking at column {c}')
+            for i in range(min(c, pattern.shape[1]-c-1)):
+                print(f'checking ({c-i},{c+i+1})')
+                if np.any(pattern[:,c-i] != pattern[:,c+i+1]):
+                    ok = False
+                    print('no match at', c)
+                    break
+            if ok:
+                print('found a match at', c)
+                return c+1
+
+    # test rows
+    for r in range(pattern.shape[0] - 1):
+        print(r)
+        if np.all(pattern[r,:] == pattern[r+1,:]):
+            ok = True
+            print(f'checking at row {r}')
+            for i in range(min(r, pattern.shape[0]-r-1)):
+                print(f'checking ({r-i},{r+i+1})')
+                if np.any(pattern[r-i,:] != pattern[r+i+1,:]):
+                    ok = False
+                    print('no match at', r)
+                    break
+            if ok:
+                print('found a match at', r)
+                return (r+1) * 100
+
+    print('no reflection!')
+    return 0
+
+def main():
+    # parse the input
+    patterns = []
+    lines = []
+    with open(sys.argv[1]) as f:
+        for line in f:
+            line = line.rstrip()
+            if line:
+                lines.append([c for c in line])
+            else:
+                patterns.append(np.array(lines))
+                lines = []
+
+    if lines:
+        patterns.append(np.array(lines))
+
+    print('Part 1:', sum(map(score, patterns)))
+
+main()
