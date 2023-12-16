0b7e382e26a2754df3bc5df7d0a53cd594618976
Walt Mankowski (macbook pro) <waltman@pobox.com>
2023-12-16 09:05:19 -0500

    removed all my debugging lines

--- day13/incidence.py
+++ day13/incidence.py
@@ -2,48 +2,36 @@ import sys
 import numpy as np
 
 def score(pattern):
-    print(f'shape = {pattern.shape}')
     nrows, ncols = pattern.shape
     
     # test rows
     for r in range(nrows - 1):
-        print('row =', r)
         if np.all(pattern[r,:] == pattern[r+1,:]):
             ok = True
-            print(f'checking at row {r}')
             for i in range(1, r+1):
                 r0, r1 = r-i, r+i+1
                 if r0 < 0 or r1 >= nrows:
                     break
-                print(f'checking rows ({r0},{r1})')
                 if np.any(pattern[r0,:] != pattern[r1,:]):
                     ok = False
-                    print('false match at', r)
                     break
             if ok:
-                print('found a row match at', r)
                 return (r+1) * 100
 
     # test columns
     for c in range(ncols - 1):
-        print('col =', c)
         if np.all(pattern[:,c] == pattern[:,c+1]):
             ok = True
-            print(f'checking at col {c}')
             for i in range(1, c+1):
                 c0, c1 = c-i, c+i+1
                 if c0 < 0 or c1 >= ncols:
                     break
-                print(f'checking cols ({c0},{c1})')
                 if np.any(pattern[:,c0] != pattern[:,c1]):
                     ok = False
-                    print('false match at', c)
                     break
             if ok:
-                print('found a column match at', c)
                 return c+1
 
-    print('no reflection!')
     return 0
 
 def main():
