925cdba21716d7bfb6553ace359c372938625945
Walt Mankowski (macbook pro) <waltman@pobox.com>
2023-12-16 20:41:47 -0500

    python code for day 15 part 1
    This was easy!

---
+++ day15/lens_library.py
@@ -0,0 +1,17 @@
+import sys
+
+def HASH(s):
+    val = 0
+    for c in s:
+        val = ((val + ord(c)) * 17) % 256
+    return val
+
+print(HASH('HASH'))
+
+def main():
+    with open(sys.argv[1]) as f:
+        steps = f.read().rstrip().split(',')
+
+        print('Part 1:', sum([HASH(step) for step in steps]))
+
+main()
