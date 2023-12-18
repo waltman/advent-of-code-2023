36710cf6356ff74181d1f9a09d36abe464d85dfc
Walt Mankowski (macbook pro) <waltman@pobox.com>
2023-12-17 23:20:40 -0500

    added code to solve 15.2

--- day15/lens_library.py
+++ day15/lens_library.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from collections import defaultdict
 
 def HASH(s):
@@ -7,10 +8,40 @@ def HASH(s):
         val = ((val + ord(c)) * 17) % 256
     return val
 
+def add_lens(box, label, lens_val):
+    boxnum = HASH(label)
+    for i in range(len(box[boxnum])):
+        if box[boxnum][i][0] == label:
+            box[boxnum][i] = (label, lens_val)
+            return
+    box[boxnum].append((label, lens_val))
+
+def delete_lens(box, label):
+    boxnum = HASH(label)
+    for i in range(len(box[boxnum])):
+        if box[boxnum][i][0] == label:
+            box[boxnum].pop(i)
+            return
+
+def focusing_power(boxnum, lenses):
+    return sum([(boxnum+1) * (i+1) * lenses[i][1] for i in range(len(lenses))])
+
 def main():
     with open(sys.argv[1]) as f:
         steps = f.read().rstrip().split(',')
 
     print('Part 1:', sum([HASH(step) for step in steps]))
+
+    box = defaultdict(list)
+    for step_str in steps:
+        m = re.match(r'^([a-z]+)(.*)$', step_str)
+        label = m.group(1)
+        if m.group(2)[0] == '=':
+            lens_val = int(m.group(2)[1:])
+            add_lens(box, label, lens_val)
+        else:
+            delete_lens(box, label)
+
+    print('Part 2:', sum([focusing_power(k,v) for k,v in box.items()]))
 
 main()
