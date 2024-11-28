6c9d6755d69a0a6b2df2c3f21b70e2351d709932
Walt Mankowski <waltman@pobox.com>
2024-11-28 06:23:01 -0500

    22.1 WIP
    parsing the input

---
+++ day22/sand_slabs.py
@@ -0,0 +1,40 @@
+import sys
+import numpy as np
+
+class Brick:
+    def __init__(self, name, p0, p1):
+        self.name = name
+        self.x0 = p0[0]
+        self.y0 = p0[1]
+        self.z0 = p0[2]
+        self.x1 = p1[0]
+        self.y1 = p1[1]
+        self.z1 = p1[2]
+
+        if self.x0 != self.x1:
+            self.direction = 'x'
+        elif self.y0 != self.y1:
+            self.direction = 'y'
+        else:
+            self.direction = 'z'
+
+    def __repr__(self):
+        return f'{self.name}: ({self.x0}, {self.y0}, {self.z0}), ({self.x1}, {self.y1}, {self.z1}) {self.direction}'
+
+def main():
+    bricks = []
+
+    # parse the input
+    with open(sys.argv[1]) as f:
+        cnt = 0
+        for line in f:
+            ends = line.rstrip().split('~')
+            p0 = [int(x) for x in ends[0].split(',')]
+            p1 = [int(x) for x in ends[1].split(',')]
+            bricks.append(Brick(cnt, p0, p1))
+            cnt += 1
+            
+    for brick in bricks:
+        print(brick)
+main()
+
