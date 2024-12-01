920613aca60aaf1018d52beb5f75d8c7f9ebdf5a
Walt Mankowski <waltman@pobox.com>
2024-12-01 17:42:13 -0500

    WIP for day 25
    This works for the test input, but it's very slow for the real input.

---
+++ day25/snowverload.py
@@ -0,0 +1,30 @@
+import sys
+import networkx as nx
+from itertools import combinations
+from copy import deepcopy
+
+def main():
+    # parse the input and turn it into an undirected graph
+    G = nx.Graph()
+
+    with open(sys.argv[1]) as f:
+        for line in f:
+            toks1 = line.rstrip().split(': ')
+            toks2 = toks1[1].split(' ')
+            for x in toks2:
+                G.add_edge(toks1[0], x)
+
+    for e1, e2, e3 in combinations(G.edges, 3):
+        G2 = deepcopy(G)
+        G2.remove_edge(e1[0], e1[1])
+        G2.remove_edge(e2[0], e2[1])
+        G2.remove_edge(e3[0], e3[1])
+        if nx.number_connected_components(G2) == 2:
+            print(e1, e2, e3)
+            prod = 1
+            for cc in nx.connected_components(G2):
+                prod *= len(cc)
+            print('Part 1:', prod)
+            break
+
+main()
