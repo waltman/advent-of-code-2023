987aa9d1d0a5702eb41af7b080fabc8d9b0c38fb
Walt Mankowski <waltman@pobox.com>
2024-12-02 20:00:41 -0500

    WIP removed deepcopy
    This code removed the call to deepcopy(). Instead I just remove the 3
    edges, then add them back if it wasn't the right edges.

--- day25/snowverload.py
+++ day25/snowverload.py
@@ -1,7 +1,6 @@
 import sys
 import networkx as nx
 from itertools import combinations
-from copy import deepcopy
 
 def main():
     # parse the input and turn it into an undirected graph
@@ -15,16 +14,19 @@ def main():
                 G.add_edge(toks1[0], x)
 
     for e1, e2, e3 in combinations(G.edges, 3):
-        G2 = deepcopy(G)
-        G2.remove_edge(e1[0], e1[1])
-        G2.remove_edge(e2[0], e2[1])
-        G2.remove_edge(e3[0], e3[1])
-        if nx.number_connected_components(G2) == 2:
+        G.remove_edge(e1[0], e1[1])
+        G.remove_edge(e2[0], e2[1])
+        G.remove_edge(e3[0], e3[1])
+        if nx.number_connected_components(G) == 2:
             print(e1, e2, e3)
             prod = 1
-            for cc in nx.connected_components(G2):
+            for cc in nx.connected_components(G):
                 prod *= len(cc)
             print('Part 1:', prod)
             break
+        else:
+            G.add_edge(e1[0], e1[1])
+            G.add_edge(e2[0], e2[1])
+            G.add_edge(e3[0], e3[1])
 
 main()
